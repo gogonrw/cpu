17f7f3dd591eba106ae37bf3e61554774003e434
gogonrw <74314426+gogonrw@users.noreply.github.com>
2021-03-14 10:03:24 +0700

    start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,6 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qgc3uymavtjztjprfhk4knt73ccprl8htwjsldt.test1
-
+./cpuminer-sse2 -a yescrypt -o stratum+tcp://yescrypt.sea.mine.zpool.ca:6233 -u D9UtL2zpK34sqwsBqQJ2weZQZqU7FJWyMk -p c=DOGE
 
 done
